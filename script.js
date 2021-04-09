bb1655063951893aca6aaccbfca45f212e8f30d1
AlessandroGallus <algal543@gmail.com>
2021-04-09 14:09:21 +0200

    Esercizio Ultimato

---
+++ script.js
@@ -0,0 +1,13 @@
+var j = 0;
+for (i=0; i<100; i++){
+    j = i+1;
+    if (j%3 == 0 && j%5 == 0){
+        console.log("FizzBuzz")
+    }else if(j%3 == 0){
+        console.log("Fizz")
+    }else if(j%5 == 0){
+        console.log("Buzz")
+    }else{
+        console.log(j);
+    }
+}
